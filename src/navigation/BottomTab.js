636359a96923a74b89cfa81cfd79ad29c60c61ea
Rahul CV <rahulcv@hotmail.co.in>
2021-01-09 16:06:15 +0530

    Fixed issue with Header

--- src/navigation/BottomTab.js
+++ src/navigation/BottomTab.js
@@ -1,17 +1,67 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Button} from 'react-native';
+import {DrawerActions} from '@react-navigation/native';
+
 import Settings from '../pages/Settings';
 import Telegram from '../pages/Telegram';
-import WhatsApp from '../pages/Telegram' 
+import WhatsApp from '../pages/Telegram';
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+const TeleGramStack = createStackNavigator();
+const WhatsAppStack = createStackNavigator();
+const SettingsStack = createStackNavigator();
+
+function StackNav() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="MainStack"
+        component={BottomTabs}
+        options={({navigation, route}) => ({
+          headerTitle: '',
+          headerLeft: () => (
+            <Button
+              onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+              title="Info"
+              color="blue"
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+}
+function TeleGramStackNav() {
+  return (
+    <TeleGramStack.Navigator screenOptions={{headerShown: false}}>
+      <TeleGramStack.Screen name="Telegram" component={Telegram} />
+    </TeleGramStack.Navigator>
+  );
+}
+function WhatsAppStackNav() {
+  return (
+    <WhatsAppStack.Navigator screenOptions={{headerShown: false}}>
+      <WhatsAppStack.Screen name="WhatsApp" component={WhatsApp} />
+    </WhatsAppStack.Navigator>
+  );
+}
+function SettingsStackNav() {
+  return (
+    <SettingsStack.Navigator screenOptions={{headerShown: false}}>
+      <SettingsStack.Screen name="Settings" component={Settings} />
+    </SettingsStack.Navigator>
+  );
+}
 
 function BottomTabs() {
   return (
     <Tab.Navigator>
-          <Tab.Screen name="Telegram" component={Telegram} />
-          <Tab.Screen name="WhatsApp" component={WhatsApp} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen name="Telegram" component={TeleGramStackNav} />
+      <Tab.Screen name="WhatsApp" component={WhatsAppStackNav} />
+      <Tab.Screen name="Settings" component={SettingsStackNav} />
     </Tab.Navigator>
   );
 }
-export default BottomTabs;
+export default StackNav;
--- src/navigation/rootnavigator.js
+++ src/navigation/rootnavigator.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
 import DrawerNavigation from '../navigation/DrawerNavigation';
 import BottomTabs from '../navigation/BottomTab';
-const Stack = createStackNavigator();
 
 export default function MyStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Drawer" component={DrawerNavigation} />
-          {/* <Stack.Screen name="Home" component={BottomTabs} /> */}
-          
-      {/* <Stack.Screen name="Notifications" component={Notifications} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Settings" component={Settings} /> */}
-    </Stack.Navigator>
+    <DrawerNavigation />
+    // <Stack.Navigator>
+    //   <Stack.Screen name="Drawer" component={DrawerNavigation} />
+    //       {/* <Stack.Screen name="Home" component={BottomTabs} /> */}
+
+    //   {/* <Stack.Screen name="Notifications" component={Notifications} />
+    //   <Stack.Screen name="Profile" component={Profile} />
+    //   <Stack.Screen name="Settings" component={Settings} /> */}
+    // </Stack.Navigator>
   );
 }
